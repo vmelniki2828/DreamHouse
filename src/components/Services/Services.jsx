dcfe06d4ed11561ca135533708d84c18027bf5b3
Ponedilok Danyyl <danya253545@gmail.com>
2025-02-16 23:49:11 +0200

    service update

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,9 +1,65 @@
-import { ServicesMainContainer, ServicesMainTitle } from './Services.styled';
+import {
+  ArrowItem,
+  ImgItem,
+  ItemButton,
+  ItemButtonBlock,
+  ItemMainText,
+  ServiceBlock,
+  ServiceItem,
+  ServiceItemLink,
+  ServiceList,
+  ServicesMainContainer,
+  ServicesMainTitle,
+  TextItem,
+  TextItemBlock,
+} from './Services.styled';
+import build_img from '../../images/img_second_service_test.png';
 
 const Services = () => {
   return (
     <ServicesMainContainer>
       <ServicesMainTitle>УСЛУГИ</ServicesMainTitle>
+      <ServiceBlock>
+        <ServiceList>
+          <ServiceItem>
+            <ItemMainText>Общестроительные работы</ItemMainText>
+            <ItemButtonBlock>
+              <ItemButton>Заказать рабочих</ItemButton>
+            </ItemButtonBlock>
+          </ServiceItem>
+          <ServiceItem>
+            <ItemMainText>Общестроительные работы</ItemMainText>
+            <ItemButtonBlock>
+              <ItemButton>Заказать рабочих</ItemButton>
+            </ItemButtonBlock>
+          </ServiceItem>
+          <ServiceItem>
+            <ItemMainText>Общестроительные работы</ItemMainText>
+            <ItemButtonBlock>
+              <ItemButton>Заказать рабочих</ItemButton>
+            </ItemButtonBlock>
+          </ServiceItem>
+          <ServiceItem>
+            <ItemMainText>Общестроительные работы</ItemMainText>
+            <ItemButtonBlock>
+              <ItemButton>Заказать рабочих</ItemButton>
+            </ItemButtonBlock>
+          </ServiceItem>
+          <ServiceItem>
+            <ItemMainText>Общестроительные работы</ItemMainText>
+            <ItemButtonBlock>
+              <ItemButton>Заказать рабочих</ItemButton>
+            </ItemButtonBlock>
+          </ServiceItem>
+          <ServiceItemLink>
+            <ImgItem src={build_img} alt="house" />
+            <TextItemBlock>
+              <TextItem>Оставить заявку на работы</TextItem>
+              <ArrowItem />
+            </TextItemBlock>
+          </ServiceItemLink>
+        </ServiceList>
+      </ServiceBlock>
     </ServicesMainContainer>
   );
 };
